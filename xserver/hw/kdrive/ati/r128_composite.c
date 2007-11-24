63b8395159799536294e8ae37a0d109ebc9f4c02
matthieu <matthieu@openbsd.org>
2007-11-24 17:55:21 +0000

    xserver 1.4

--- xserver/hw/kdrive/ati/r128_composite.c
+++ xserver/hw/kdrive/ati/r128_composite.c
@@ -19,7 +19,6 @@
  * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  * PERFORMANCE OF THIS SOFTWARE.
  */
-/* $Header: /cvs/xenocara/xserver/hw/kdrive/ati/Attic/r128_composite.c,v 1.1.1.1 2006/11/26 18:22:12 matthieu Exp $ */
 
 #include "ati.h"
 #include "ati_reg.h"
